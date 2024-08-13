3e9541762c628b3cb1408d08955fe3a2eac98b7c
Mike Vincent <nonanom@example.com>
2024-08-13 11:28:40 -0700

    update

--- terraform/aws/rds.tf
+++ terraform/aws/rds.tf
@@ -12,7 +12,7 @@ resource "aws_vpc" "rds_vpc" {
 # Create two private subnets for RDS in different AZs
 resource "aws_subnet" "rds_private_1" {
   vpc_id            = aws_vpc.rds_vpc.id
-  cidr_block        = "10.0.1.0/24"
+  cidr_block        = "10.0.3.0/24"
   availability_zone = "${var.AWS_DEFAULT_REGION}a"
 
   tags = {
@@ -22,7 +22,7 @@ resource "aws_subnet" "rds_private_1" {
 
 resource "aws_subnet" "rds_private_2" {
   vpc_id            = aws_vpc.rds_vpc.id
-  cidr_block        = "10.0.2.0/24"
+  cidr_block        = "10.0.4.0/24"
   availability_zone = "${var.AWS_DEFAULT_REGION}b"
 
   tags = {
